c1509cbb02beb08e721a87b88ff2abd1b0eb7f1c
Aaron Fletcher <afletcher53@gmail.com>
2023-11-07 14:45:40 +0000

    Refactor argument parsing and linting in `lint.py`
    - Refactored the argument parsing code to use a shorter syntax
    - Simplified the linting process by removing unnecessary logging statements
    - Updated the output and exit handling logic for better readability
    
    This commit simplifies the argument parsing and linting process in `lint.py`. The code now uses a shorter syntax for argument parsing, making it more concise. Additionally, unnecessary logging statements have been removed to improve clarity.
    
    The output and exit handling logic has also been updated for better readability. The final score is now obtained directly from the pylint output, eliminating the need for intermediate variables. If the score is below the specified threshold, an error message is printed along with the pylint output. Otherwise, a success message is printed.
    
    These changes make the code more streamlined and easier to understand.
    Refactor argument parsing and linting in `lint.py`
    
    - Refactored the argument parsing code to use a shorter syntax
    - Simplified the linting process by removing unnecessary logging statements
    - Updated the output and exit handling logic for better readability
    
    This commit simplifies the argument parsing and linting process in `lint.py`. The code now uses a shorter syntax for argument parsing, making it more concise. Additionally, unnecessary logging statements have been removed to improve clarity.
    
    The output and exit handling logic has also been updated for better readability. The final score is now obtained directly from the pylint output, eliminating the need for intermediate variables. If the score is below the specified threshold, an error message is printed along with the pylint output. Otherwise, a success message is printed.
    
    These changes make the code more streamlined and easier to understand.

--- lint.py
+++ lint.py
@@ -4,55 +4,21 @@ Usage: $ python lint.py --path ./my_project --threshold 8.5
 """
 
 import argparse
-import logging
-import sys
 from io import StringIO
 from pylint.lint import Run
 from pylint.reporters.text import TextReporter
 
-logging.getLogger().setLevel(logging.INFO)
-
-parser = argparse.ArgumentParser(prog="LINT")
-
-parser.add_argument(
-    "-p",
-    "--path",
-    help="path to directory you want to run pylint | "
-    "Default: %(default)s | "
-    "Type: %(type)s ",
-    default="./src",
-    type=str,
-)
-
-parser.add_argument(
-    "-t",
-    "--threshold",
-    help="score threshold to fail pylint runner | "
-    "Default: %(default)s | "
-    "Type: %(type)s ",
-    default=7,
-    type=float,
-)
-
+# Argument parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--path", default="./src", type=str)
+parser.add_argument("-t", "--threshold", default=7, type=float)
 args = parser.parse_args()
-PATH = str(args.path)
-THRESHOLD = float(args.threshold)
 
-logging.info("PyLint Starting | Path: %s | Threshold: %s", PATH, THRESHOLD)
+# Linting
+pylint_output = StringIO()
+results = Run([args.path], reporter=TextReporter(pylint_output), exit=False)
 
-pylint_output = StringIO()  # Custom open stream
-reporter = TextReporter(pylint_output)
-results = Run([PATH], reporter=reporter, exit=False)
-
-
-final_score = results.linter.stats.global_note
-
-if float(final_score) < THRESHOLD:
-    message = f"PyLint Failed | Score: {final_score} | Threshold: {THRESHOLD}"
-    logging.error(message)
-    print(pylint_output.getvalue())
-    raise ValueError(message)
-else:
-    message = f"PyLint Passed | Score: {final_score} | Threshold: {THRESHOLD}"
-    logging.info(message)
-    sys.exit(0)
+if args.threshold > (score := float(results.linter.stats.global_note)):
+    print(f"PyLint Failed | Score: {score} | Threshold: {args.threshold}\n{float(results.linter.stats.global_note)}")
+    raise ValueError("PyLint check failed")
+print(f"PyLint Passed | Score: {score} | Threshold: {args.threshold}")
